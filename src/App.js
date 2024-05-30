2dfdbd34cc2d1c3dd162263fa7682691af39742b
byron garin <jcgarin@up.edu.ph>
2024-05-30 23:43:32 +0800

    feat: css added

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import {
 } from "@elastic/react-search-ui";
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import './App.css'; 
 
 import {
   buildAutocompleteQueryConfig,
